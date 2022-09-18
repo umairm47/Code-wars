04b19cd918e919e20f73038972d8a6809b59c196
Muhammad U <48363297+umairm47@users.noreply.github.com>
2022-09-18 14:14:00 -0400

    Update 7kyu-isTheStringUppercase.js

--- 7kyu-isTheStringUppercase.js
+++ 7kyu-isTheStringUppercase.js
@@ -14,7 +14,7 @@
  *   "ACSKLDFJSgSKLDFJSKLDFJ" -> False
  *   "ACSKLDFJSGSKLDFJSKLDFJ" -> True
  * Pseudocode 
-   I would convert one copy of the string to all capital and then compare the capital string with the original and return the result. if they are equal then the result would be true. if not then the result will be false.
+   I would convert one copy of the string to all capital and then compare the capital string with the original and return the result. If they are equal then the result would be true. if not then the result will be false.
  
  */
    String.prototype.isUpperCase = function() {
