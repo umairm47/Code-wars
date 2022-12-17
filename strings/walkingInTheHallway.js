8b2aa848d27df53069357992bcb7e065ea6d06f1
Muhammad U <48363297+umairm47@users.noreply.github.com>
2022-12-16 22:21:17 -0500

    Updated

--- strings/walkingInTheHallway.js
+++ strings/walkingInTheHallway.js
@@ -1,5 +1,4 @@
-/*
-You are a security guard at a large company, your job is to look over the cameras. Finding yourself bored you decide to make a game from the people walking in a hallway on one of the cameras. As many people walk past the hallway you decide to figure out the minimum steps it will take before 2 people cross or come into contact with each other (assuming every person takes a step at the same time).
+/*You are a security guard at a large company, your job is to look over the cameras. Finding yourself bored you decide to make a game from the people walking in a hallway on one of the cameras. As many people walk past the hallway you decide to figure out the minimum steps it will take before 2 people cross or come into contact with each other (assuming every person takes a step at the same time).
 
 People are represented as arrows, < for a person moving left and > for a person moving right and - for an empty space
 
