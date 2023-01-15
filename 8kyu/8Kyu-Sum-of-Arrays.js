fb9de23e68a3500fca819d5317e262edd389a0b5
Muhammad U <48363297+umairm47@users.noreply.github.com>
2023-01-14 19:27:55 -0500

    Update 8Kyu-Sum-of-Arrays.js

--- 8kyu/8Kyu-Sum-of-Arrays.js
+++ 8kyu/8Kyu-Sum-of-Arrays.js
@@ -1,5 +1,4 @@
 //Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
-
 /**
  * Parameters - a boolean
  * Return - a string
