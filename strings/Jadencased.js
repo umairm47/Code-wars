6a883a1af21427b6a57cfe7a3020bcc9c4cd16f7
Muhammad U <48363297+umairm47@users.noreply.github.com>
2023-01-17 22:03:53 -0500

    Update Jadencased.js

--- strings/Jadencased.js
+++ strings/Jadencased.js
@@ -1,5 +1,6 @@
 /*In this kata Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
 
+
 /**
  * Parameters - a string
  * Return - a string
